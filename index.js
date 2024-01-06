a2906accd4ddca82526192e618d8758b9f356b32
Bastosraphael <bastosraphaelsantos@gmail.com>
2024-01-06 21:42:56 +0000

    primeiro projeto felipao

---
+++ index.js
@@ -0,0 +1,47 @@
+let xp = "100"
+let nickName = "Raphael"
+
+switch (xp) {
+case xp++ < 1000:
+console.log("Ferro");
+break;
+
+
+case xp++ == 1001 <= 2000:
+console.log ("Bronze")
+break
+
+
+case xp++ == 2001 <= 5000:
+console.log ("Bronze")
+break
+
+case xp++ == 5001 <= 7000:
+console.log ("Prata")
+break
+
+case xp++ == 7001 <= 8000:
+console.log ("Ouro")
+break
+
+case xp++ == 8001 <= 9000:
+console.log ("Ascendente")
+break
+
+case xp++ == 9001 <= 10000:
+console.log ("Imortal")
+break
+
+case xp++ > 10000:
+console.log ("Radiante")
+break
+default:
+    console.log("O heroi de nome + nickName esta no nivel de + xp ")
+
+
+
+
+
+
+}
+
